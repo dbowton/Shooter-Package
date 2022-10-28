5d12cfcbb3cd8ddf5fe9b64ca51f4f62a5770d19
dbowton <80086095+dbowton@users.noreply.github.com>
2022-10-28 11:54:19 -0600

    Dialogue Update
    Allows for a singular save point into the conversation

--- Assets/Functional/Scripts/Dialogue/DialogueSystem.cs
+++ Assets/Functional/Scripts/Dialogue/DialogueSystem.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 public class DialogueSystem : MonoBehaviour
 {
     public Conversation startingPoint;
+    public bool forceIntroduction;
+    private bool finishedIntroduciton = false;
+    public Conversation returningPoint;
     public AudioSource audioSource;
 
     private Conversation currentConversation;
-    bool switching = false;
+    bool switching = true;
 
     public List<GameObject> UIPrefabs = new List<GameObject>();
     public List<KeyCode> optionKeyCodes = new List<KeyCode>();
@@ -16,13 +19,51 @@ public class DialogueSystem : MonoBehaviour
 
     private void Start()
     {
+        BeginConversation();
+    }
+
+    public void BeginConversation()
+    {
         currentConversation = startingPoint;
         switching = true;
         SetUI();
     }
 
+    public void ReturnToConversation()
+    {
+        if(forceIntroduction && !finishedIntroduciton)
+        {
+            BeginConversation();
+        }
+        else
+        {
+            currentConversation = returningPoint;
+            switching = true;
+            SetUI();
+        }
+    }
+
+    private bool conversationActive = true;
+
     private void Update()
     {
+        if (conversationActive && Input.GetKeyDown(KeyCode.Escape))
+        {
+            conversationActive = false;
+            currentConversation = null;
+            Destroy(currentUI);
+        }
+
+        if (!conversationActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            conversationActive = true;
+            ReturnToConversation();
+            SetUI();
+        }
+
+        if (!conversationActive) return;
+
+
         int selected = -1;
         if(!switching && (currentConversation.allowInterupts || !audioSource.isPlaying))
         {
@@ -53,13 +94,15 @@ public class DialogueSystem : MonoBehaviour
         {
             Conversation.DialogueOption chosen = currentConversation.options[selected];
             currentConversation = chosen.nextConversation;
+
+            if (currentConversation.Equals(returningPoint)) finishedIntroduciton = true;
+
             audioSource.clip = chosen.voiceResponse;
             audioSource.volume = chosen.voiceResponseVolume;
             audioSource.Play();
             switching = true;
             if (chosen.action != null) chosen.action.Invoke();
         }
-
     }
 
     void SetUI()
